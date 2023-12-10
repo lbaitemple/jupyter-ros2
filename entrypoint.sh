17ae6ebdeacb41dc7a3242e5a1f124cc03d04829
Li Bai <lbai@temple.edu>
2023-12-10 11:31:02 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source ${ROS_PATH}/setup.bash
 source /opt/ros_demos/setup.bash
-
+alias dance='ros2 launch mini_pupper_dance enhanced_dance.launch.py  hardware_connected:=false'
+alias sendcmd='ros2 topic pub --once /dance_config std_msgs/String "data: 'my_dance'"'
 exec "$@"
